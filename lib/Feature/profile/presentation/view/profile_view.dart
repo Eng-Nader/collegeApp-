444a2339b22c45155d29620071afcd68ad1704ce
NaderSameer <nadersameer@std.mans.edu.eg>
2025-08-23 22:05:01 +0300

    reFactor of Profile View

--- lib/Feature/profile/presentation/view/profile_view.dart
+++ lib/Feature/profile/presentation/view/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:college_app/Feature/auth/data/model/profile_model.dart';
+import 'package:college_app/Feature/profile/presentation/widget/profile_view_body.dart';
 import 'package:college_app/constant.dart';
 import 'package:college_app/core/assets/app_assets.dart';
 import 'package:flutter/material.dart';
@@ -36,87 +37,7 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        physics: BouncingScrollPhysics(),
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.arrow_back,
-                      ),
-                      SizedBox(
-                        width: 130,
-                      ),
-                      Text(
-                        'Profile',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CircleAvatar(
-                    backgroundImage: AssetImage(AppAssets.manLoog),
-                    radius: 70,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    'Nader Sameer',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: profileList.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.only(bottom: 16),
-                        child: Card(
-                          color: Colors.white,
-                          child: ListTile(
-                            leading: Icon(
-                              profileList[index].icon,
-                              color: primaryColor,
-                              size: 30,
-                            ),
-                            title: Text(
-                              profileList[index].text,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: ProfileViewBody(),
     );
   }
 }
